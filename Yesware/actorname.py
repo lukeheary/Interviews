68bdbfb431070424ffd394687389396a012dc5a5
lukeheary <lukeheary@gmail.com>
2019-12-10 16:37:30 -0500

    updated naming conventions

--- Yesware/actorname.py
+++ Yesware/actorname.py
@@ -3,74 +3,74 @@
 import sys
 
 def main():
-    file = sys.argv[1]
+    data_file = sys.argv[1]
     n = int(sys.argv[2])
 
     # Fastest way to input a text file, reads one line into memory at once
-    uniqueNames = set()
-    uniqueFirstNames = set()
-    uniqueLastNames = set()
-    mostCommonFirstNames = dict()
-    mostCommonLastNames = dict()
+    unique_names = set()
+    unique_first_names = set()
+    unique_last_names = set()
+    most_common_first_names = dict()
+    most_common_last_names = dict()
 
-    alreadyHaveSeen = set()
-    specificallyUnique = set()
+    already_have_seen = set()
+    specifically_unique = set()
 
-    with open(file) as FileObj:
+    with open(data_file) as FileObj:
         for count, line in enumerate(FileObj, start=1):
             # Gets every other line (because every other line is a name)
             if count % 2 == 1:
-                cleanedLine = line.replace(" ","")
-                cleanedLine = cleanedLine.split("--")
-                cleanedLine = cleanedLine[0].split(",")
-                firstName = cleanedLine[1]
-                lastName = cleanedLine[0]
-                uniqueNames.add(firstName + " " + lastName)
-                uniqueFirstNames.add(firstName)
-                uniqueLastNames.add(lastName)
+                cleaned_line = line.replace(" ","")
+                cleaned_line = cleaned_line.split("--")
+                cleaned_line = cleaned_line[0].split(",")
+                first_name = cleaned_line[1]
+                last_name = cleaned_line[0]
+                unique_names.add(first_name + " " + last_name)
+                unique_first_names.add(first_name)
+                unique_last_names.add(last_name)
 
                 # These two if statements figure out the most common First and Last names
-                if firstName not in mostCommonFirstNames:
-                    mostCommonFirstNames[firstName] = 0
+                if first_name not in most_common_first_names:
+                    most_common_first_names[first_name] = 0
                 else:
-                    mostCommonFirstNames[firstName] += 1
+                    most_common_first_names[first_name] += 1
 
-                if lastName not in mostCommonLastNames:
-                    mostCommonLastNames[lastName] = 0
+                if last_name not in most_common_last_names:
+                    most_common_last_names[last_name] = 0
                 else:
-                    mostCommonLastNames[lastName] += 1
+                    most_common_last_names[last_name] += 1
 
                 # This set of if statements figures out if the names are specifically unique
-                if firstName not in alreadyHaveSeen:
-                    if lastName not in alreadyHaveSeen:
-                        specificallyUnique.add(firstName + " " + lastName)
+                if first_name not in already_have_seen:
+                    if last_name not in already_have_seen:
+                        specifically_unique.add(first_name + " " + last_name)
                     else:
-                        alreadyHaveSeen.add(lastName)
+                        already_have_seen.add(last_name)
                 else:
-                    alreadyHaveSeen.add(firstName)
+                    already_have_seen.add(first_name)
 
     # Gets the most common first name, and the how many times it shows up
-    mostCommonFirstName = max(mostCommonFirstNames, key=mostCommonFirstNames.get)
-    mostCommonFirstNameCount = str(mostCommonFirstNames[mostCommonFirstName])
+    most_common_first_name = max(most_common_first_names, key=most_common_first_names.get)
+    most_common_first_name_count = str(most_common_first_names[most_common_first_name])
 
     # Gets the most common last name, and the how many times it shows up
-    mostCommonLastName = max(mostCommonLastNames, key=mostCommonLastNames.get)
-    most_common_last_name_count = str(mostCommonLastNames[mostCommonLastName])
+    most_common_last_name = max(most_common_last_names, key=most_common_last_names.get)
+    most_common_last_name_count = str(most_common_last_names[most_common_last_name])
 
     # Gets the n amount of specifically unique names and appends them to a string for printing
-    specificallyUniqueList = list(specificallyUnique)
-    specificallyUniqueString = ""
+    specifically_unique_list = list(specifically_unique)
+    specifically_unique_string = ""
     for i in range(n):
-        specificallyUniqueString += specificallyUniqueList[i] + ", "
+        specifically_unique_string += specifically_unique_list[i] + ", "
 
-    print "1. Unique Count of Full Names: " + str(len(uniqueNames))
-    print "2. Unique Count of First Names: " + str(len(uniqueFirstNames))
-    print "3. Unique Count of Last Names: " + str(len(uniqueLastNames))
-    print "4. Most Common First Name: " + mostCommonFirstName + " (appears " + mostCommonFirstNameCount + " times)"
-    print "5. Most Common Last Name: " + mostCommonLastName + " (appears " + most_common_last_name_count + " times)"
+    print "1. Unique Count of Full Names: " + str(len(unique_names))
+    print "2. Unique Count of First Names: " + str(len(unique_first_names))
+    print "3. Unique Count of Last Names: " + str(len(unique_last_names))
+    print "4. Most Common First Name: " + most_common_first_name + " (appears " + most_common_first_name_count + " times)"
+    print "5. Most Common Last Name: " + most_common_last_name + " (appears " + most_common_last_name_count + " times)"
 
 
-    print "6. " + str(n) + " Specifically Unique Names: " + specificallyUniqueString
+    print "6. " + str(n) + " Specifically Unique Names: " + specifically_unique_string
     print "7. "
 
 main()
